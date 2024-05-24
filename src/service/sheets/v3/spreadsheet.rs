cec4d2d5587ffc6a42575378fd212645375ed3a6
ZoOL <zhooul@gmail.com>
2024-05-24 10:58:46 +0800

    修改电子表格属性

--- src/service/sheets/v3/spreadsheet.rs
+++ src/service/sheets/v3/spreadsheet.rs
@@ -9,6 +9,7 @@ use crate::core::{
     req_option::RequestOption,
     SDKResult,
 };
+use crate::core::api_resp::RawResponse;
 
 pub struct SpreadsheetService {
     config: Config,
@@ -28,6 +29,21 @@ impl SpreadsheetService {
         let mut api_req = request.api_request;
         api_req.http_method = "POST".to_string();
         api_req.api_path = "/open-apis/sheets/v3/spreadsheets".to_string();
+        api_req.supported_access_token_types = vec![AccessTokenType::Tenant, AccessTokenType::User];
+
+        let api_resp = Transport::request(api_req, &self.config, option)?;
+
+        Ok(api_resp)
+    }
+    /// 修改电子表格属性
+    pub fn patch(
+        &self,
+        request: PatchSpreadSheetRequest,
+        option: Option<RequestOption>,
+    ) -> SDKResult<ApiResponse<RawResponse>> {
+        let mut api_req = request.api_request;
+        api_req.http_method = "PATCH".to_string();
+        api_req.api_path = format!("/open-apis/sheets/v3/spreadsheets/{}", request.spreadsheet_token);
         api_req.supported_access_token_types = vec![AccessTokenType::Tenant, AccessTokenType::User];
 
         let api_resp = Transport::request(api_req, &self.config, option)?;
@@ -83,7 +99,8 @@ impl CreateSpreedSheetRequestBuilder {
         self
     }
 
-    pub fn build(self) -> CreateSpreedSheetRequest {
+    pub fn build(mut self) -> CreateSpreedSheetRequest {
+        self.request.api_request.body = serde_json::to_vec(&self.request).unwrap();
         self.request
     }
 }
@@ -111,6 +128,48 @@ pub struct CreateSpreedSheetResponse {
     url: String,
     /// 表格token
     spreadsheet_token: String,
+}
+
+/// 修改电子表格属性 请求体
+#[derive(Default, Debug, Serialize)]
+pub struct PatchSpreadSheetRequest {
+    #[serde(skip)]
+    api_request: ApiRequest,
+    /// 表格的token
+    #[serde(skip)]
+    spreadsheet_token: String,
+    /// 表格标题
+    title: String,
+}
+
+impl PatchSpreadSheetRequest {
+    pub fn builder() -> PatchSpreadSheetRequestBuilder {
+        PatchSpreadSheetRequestBuilder::default()
+    }
+}
+
+#[derive(Default)]
+pub struct PatchSpreadSheetRequestBuilder {
+    request: PatchSpreadSheetRequest,
+}
+
+impl PatchSpreadSheetRequestBuilder {
+    /// 表格标题
+    pub fn title(mut self, title: impl ToString) -> Self {
+        self.request.title = title.to_string();
+        self
+    }
+
+    /// 表格的token
+    pub fn spreadsheet_token(mut self, spreadsheet_token: impl ToString) -> Self {
+        self.request.spreadsheet_token = spreadsheet_token.to_string();
+        self
+    }
+
+    pub fn build(mut self) -> PatchSpreadSheetRequest {
+        self.request.api_request.body = serde_json::to_vec(&self.request).unwrap();
+        self.request
+    }
 }
 
 #[cfg(test)]
