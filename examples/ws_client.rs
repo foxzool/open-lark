01769134760d6a064e457b85c95b88d0aad3c85a
ZoOL <zhooul@gmail.com>
2024-05-27 10:50:35 +0800

    websocket 连接初步

--- examples/ws_client.rs
+++ examples/ws_client.rs
@@ -9,6 +9,6 @@ async fn main() {
     let app_id = env::var("APP_ID").unwrap();
     let app_secret = env::var("APP_SECRET").unwrap();
 
-    let client = LarkWsClient::new(app_id, app_secret);
+    let mut client = LarkWsClient::new(app_id, app_secret);
     client.start().await.unwrap();
 }
--- src/client/ws.rs
+++ src/client/ws.rs
@@ -1,6 +1,14 @@
-use crate::core::{api_resp::BaseResponse, constants::FEISHU_BASE_URL, SDKResult};
+use std::collections::HashMap;
+
+use futures_util::{future, pin_mut, StreamExt};
+use log::debug;
 use serde::Deserialize;
 use serde_json::json;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use url::Url;
+
+use crate::core::{api_resp::BaseResponse, constants::FEISHU_BASE_URL};
 
 const END_POINT_URL: &str = "/callback/ws/endpoint";
 
@@ -10,6 +18,13 @@ pub struct LarkWsClient {
     app_secret: String,
     auto_reconnect: bool,
     domain: String,
+    reconnect_count: i32,
+    reconnect_interval: i32,
+    reconnect_nonce: i32,
+    ping_interval: i32,
+    conn_url: String,
+    conn_id: String,
+    service_id: String,
 }
 
 impl LarkWsClient {
@@ -19,18 +34,54 @@ impl LarkWsClient {
             app_secret: app_secret.to_string(),
             auto_reconnect: true,
             domain: FEISHU_BASE_URL.to_string(),
+            reconnect_count: 30,
+            reconnect_interval: -1,
+            reconnect_nonce: 2 * 60,
+            ping_interval: 2 * 60,
+            conn_url: "".to_string(),
+            conn_id: "".to_string(),
+            service_id: "".to_string(),
         }
     }
 
-    pub async fn start(&self) -> SDKResult<()> {
+    pub async fn start(&mut self) -> WsResult<()> {
         self.connect().await
     }
 
-    async fn connect(&self) -> SDKResult<()> {
-        self.get_conn().await
+    async fn connect(&mut self) -> WsResult<()> {
+        let conn_url = self.get_conn().await?;
+        let url = Url::parse(&conn_url)?;
+
+        let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
+        tokio::spawn(read_stdin(stdin_tx));
+
+        let query_pairs: HashMap<_, _> = url.query_pairs().into_iter().collect();
+        let conn_id = query_pairs.get("device_id").unwrap().to_string();
+        let service_id = query_pairs.get("device_id").unwrap().to_string();
+        self.conn_id = conn_id;
+        self.service_id = service_id;
+        self.conn_url = url.to_string();
+
+        let (ws_stream, _response) = connect_async(url).await?;
+        let (write, read) = ws_stream.split();
+
+
+
+        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
+        let ws_to_stdout = {
+            read.for_each(|message| async {
+                let data = message.unwrap().into_data();
+                tokio::io::stdout().write_all(&data).await.unwrap();
+            })
+        };
+
+        pin_mut!(stdin_to_ws, ws_to_stdout);
+        future::select(stdin_to_ws, ws_to_stdout).await;
+
+        Ok(())
     }
 
-    async fn get_conn(&self) -> SDKResult<()> {
+    async fn get_conn(&mut self) -> WsResult<String> {
         let body = json!({
             "AppID": self.app_id,
             "AppSecret": self.app_secret
@@ -38,24 +89,73 @@ impl LarkWsClient {
 
         let req = reqwest::Client::new()
             .post(&format!("{}{END_POINT_URL}", self.domain))
+            .header("locale", "zh")
             .json(&body)
             .send()
             .await?;
 
-        // println!("{:?}", req.text().await.unwrap());
         let resp = req.json::<BaseResponse<EndPointResponse>>().await?;
-        println!("{:?}", resp.data);
+        debug!("{:?}", resp.data);
 
-        Ok(())
+        if !resp.success() {
+            return match resp.raw_response.code {
+                1 => Err(WsClientError::ServerError {
+                    code: resp.raw_response.code,
+                    message: resp.raw_response.msg,
+                }),
+                1000040343 => Err(WsClientError::ServerError {
+                    code: resp.raw_response.code,
+                    message: resp.raw_response.msg,
+                }),
+                _ => Err(WsClientError::ClientError {
+                    code: resp.raw_response.code,
+                    message: resp.raw_response.msg,
+                }),
+            };
+        }
+
+        let end_point = resp.data.unwrap();
+        if end_point.url.is_none() || end_point.url.as_ref().unwrap().is_empty() {
+            return Err(WsClientError::ServerError {
+                code: 500,
+                message: "No available endpoint".to_string(),
+            });
+        }
+
+        if let Some(client_config) = end_point.client_config {
+            self.configure(client_config)
+        }
+
+        Ok(end_point.url.unwrap())
+    }
+
+    fn configure(&mut self, config: ClientConfig) {
+        self.reconnect_count = config.reconnect_count;
+        self.reconnect_interval = config.reconnect_interval;
+        self.reconnect_nonce = config.reconnect_nonce;
+        self.ping_interval = config.ping_interval;
+    }
+}
+
+async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
+    let mut stdin = tokio::io::stdin();
+    loop {
+        let mut buf = vec![0; 1024];
+        let n = match stdin.read(&mut buf).await {
+            Err(_) | Ok(0) => break,
+            Ok(n) => n,
+        };
+        buf.truncate(n);
+        tx.unbounded_send(Message::binary(buf)).unwrap();
     }
 }
 
 #[derive(Debug, Deserialize)]
 pub struct EndPointResponse {
     #[serde(rename = "URL")]
-    pub url: String,
+    pub url: Option<String>,
     #[serde(rename = "ClientConfig")]
-    pub client_config: ClientConfig,
+    pub client_config: Option<ClientConfig>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -68,4 +168,20 @@ pub struct ClientConfig {
     reconnect_nonce: i32,
     #[serde(rename = "PingInterval")]
     ping_interval: i32,
+}
+
+pub type WsResult<T> = Result<T, WsClientError>;
+
+#[derive(Debug, thiserror::Error)]
+pub enum WsClientError {
+    #[error("Request error: {0}")]
+    RequestError(#[from] reqwest::Error),
+    #[error("Url parse error: {0}")]
+    UrlParseError(#[from] url::ParseError),
+    #[error("Server error: {code}, {message}")]
+    ServerError { code: i32, message: String },
+    #[error("Client error: {code}, {message}")]
+    ClientError { code: i32, message: String },
+    #[error("WebSocket error: {0}")]
+    WsError(#[from] tokio_tungstenite::tungstenite::Error),
 }
