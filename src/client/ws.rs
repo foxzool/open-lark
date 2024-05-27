934b388d7da1dfb92ab44d51a6d9dc719ae77636
ZoOL <zhooul@gmail.com>
2024-05-27 12:06:39 +0800

    读取message

--- src/client/ws.rs
+++ src/client/ws.rs
@@ -1,11 +1,14 @@
 use std::collections::HashMap;
+use futures_util::{SinkExt, StreamExt};
 
-use futures_util::{future, pin_mut, StreamExt};
+
+use kanal::{AsyncReceiver, AsyncSender};
 use log::debug;
 use serde::Deserialize;
 use serde_json::json;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{connect_async, MaybeTlsStream, tungstenite::protocol::Message, WebSocketStream};
 use url::Url;
 
 use crate::core::{api_resp::BaseResponse, constants::FEISHU_BASE_URL};
@@ -25,6 +28,7 @@ pub struct LarkWsClient {
     conn_url: String,
     conn_id: String,
     service_id: String,
+    sender_tx: Option<AsyncSender<Message>>,
 }
 
 impl LarkWsClient {
@@ -41,6 +45,7 @@ impl LarkWsClient {
             conn_url: "".to_string(),
             conn_id: "".to_string(),
             service_id: "".to_string(),
+            sender_tx: None,
         }
     }
 
@@ -49,11 +54,8 @@ impl LarkWsClient {
     }
 
     async fn connect(&mut self) -> WsResult<()> {
-        let conn_url = self.get_conn().await?;
+        let conn_url = self.get_conn_url().await?;
         let url = Url::parse(&conn_url)?;
-
-        let (stdin_tx, stdin_rx) = futures_channel::mpsc::unbounded();
-        tokio::spawn(read_stdin(stdin_tx));
 
         let query_pairs: HashMap<_, _> = url.query_pairs().into_iter().collect();
         let conn_id = query_pairs.get("device_id").unwrap().to_string();
@@ -63,25 +65,41 @@ impl LarkWsClient {
         self.conn_url = url.to_string();
 
         let (ws_stream, _response) = connect_async(url).await?;
-        let (write, read) = ws_stream.split();
+        let (mut write, mut read) = ws_stream.split();
+        let (sender_tx, sender_rx) = kanal::unbounded_async::<Message>();
 
 
-
-        let stdin_to_ws = stdin_rx.map(Ok).forward(write);
-        let ws_to_stdout = {
-            read.for_each(|message| async {
-                let data = message.unwrap().into_data();
-                tokio::io::stdout().write_all(&data).await.unwrap();
-            })
+        let write_task = async move {
+            while let Ok(msg) = sender_rx.recv().await {
+                write.send( msg).await.unwrap();
+            }
         };
 
-        pin_mut!(stdin_to_ws, ws_to_stdout);
-        future::select(stdin_to_ws, ws_to_stdout).await;
+        self.sender_tx = Some(sender_tx);
+
+        let read_task = async move {
+            while let Some(message) = read.next().await {
+                match message {
+                    Ok(msg) => {
+                        self.handle_message(msg);
+                    }
+                    Err(e) => {
+                        println!("Error: {:?}", e);
+                    }
+                }
+            }
+        };
+
+        tokio::select! {
+            _ = write_task => {}
+            _ = read_task => {}
+        }
 
         Ok(())
     }
 
-    async fn get_conn(&mut self) -> WsResult<String> {
+    /// 获取连接地址
+    async fn get_conn_url(&mut self) -> WsResult<String> {
         let body = json!({
             "AppID": self.app_id,
             "AppSecret": self.app_secret
@@ -129,26 +147,60 @@ impl LarkWsClient {
         Ok(end_point.url.unwrap())
     }
 
+    /// 配置
     fn configure(&mut self, config: ClientConfig) {
         self.reconnect_count = config.reconnect_count;
         self.reconnect_interval = config.reconnect_interval;
         self.reconnect_nonce = config.reconnect_nonce;
         self.ping_interval = config.ping_interval;
     }
-}
 
-async fn read_stdin(tx: futures_channel::mpsc::UnboundedSender<Message>) {
-    let mut stdin = tokio::io::stdin();
-    loop {
-        let mut buf = vec![0; 1024];
-        let n = match stdin.read(&mut buf).await {
-            Err(_) | Ok(0) => break,
-            Ok(n) => n,
-        };
-        buf.truncate(n);
-        tx.unbounded_send(Message::binary(buf)).unwrap();
+    fn handle_message(&self, message: Message) {
+        match message {
+            Message::Text(text) => {
+                debug!("Received a text message: {:?}", text);
+            }
+            Message::Binary(bin) => {
+                debug!("Received a binary message: {:?}", bin);
+            }
+            Message::Ping(ping) => {
+                debug!("Received a ping message {:?}", ping);
+            }
+            Message::Pong(pong) => {
+                debug!("Received a pong message");
+                let config: ClientConfig = serde_json::from_slice(&pong).unwrap();
+                debug!("{:?}", config);
+            }
+            Message::Close(close) => {
+                debug!("Received a close message: {:?}", close);
+            }
+            Message::Frame(_) => {}
+        }
+    }
+
+    fn ping_loop(&mut self) {
+        let ping_interval = self.ping_interval;
+        let conn_id = self.conn_id.clone();
+        let service_id = self.service_id.clone();
+        let conn_url = self.conn_url.clone();
+        if let Some(msg_tx) = self.sender_tx.clone() {
+            tokio::spawn(async move {
+                loop {
+                    tokio::time::sleep(tokio::time::Duration::from_secs(ping_interval as u64)).await;
+                    let body = json!({
+                        "device_id": conn_id,
+                        "service_id": service_id
+                    });
+                    let msg = Message::Binary(serde_json::to_string(&body).unwrap().into());
+                    msg_tx.send(msg).await.unwrap();
+                }
+            });
+        }
     }
 }
+
+
+
 
 #[derive(Debug, Deserialize)]
 pub struct EndPointResponse {
