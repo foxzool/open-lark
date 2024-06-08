a92fdbe5b892ae2cc7094475fc8850e2c790e777
John <admin@iios.net>
2024-06-09 06:36:48 +0800

    update client using lifetime for str instead of to_string

--- examples/ws_client.rs
+++ examples/ws_client.rs
@@ -9,7 +9,7 @@ async fn main() {
     let app_id = env::var("APP_ID").unwrap();
     let app_secret = env::var("APP_SECRET").unwrap();
 
-    let client = LarkWsClient::new(app_id, app_secret);
+    let client = LarkWsClient::new(&app_id, &app_secret);
 
     client.start().await.unwrap();
 }
