96505627e1efe348b9b259a859cf31c493d387a1
ZoOL <zhooul@gmail.com>
2024-06-18 20:41:07 +0800

    电子表格 - 拆分单元格

---
+++ examples/api/sheets/v2/data-operation/split_cells.rs
@@ -0,0 +1,33 @@
+use std::env;
+
+use dotenvy::dotenv;
+
+use open_lark::{
+    client::LarkClientBuilder, service::sheets::v2::data_operation::SplitCellsRequest,
+};
+
+/// 拆分单元格
+#[tokio::main]
+async fn main() {
+    dotenv().expect(".env file not found");
+    env_logger::init();
+    let app_id = env::var("APP_ID").unwrap();
+    let app_secret = env::var("APP_SECRET").unwrap();
+    // 创建 Client
+    let client = LarkClientBuilder::new(&app_id, &app_secret).build();
+
+    let req = SplitCellsRequest::builder()
+        .spreadsheet_token("O21wsTInWht7sUtRj77cFwRXnme")
+        .range("0ae03b!G2:G3")
+        .build();
+    let resp = client.sheets.v2.spreadsheet.split_cells(req, None).await;
+
+    match resp {
+        Ok(base_resp) => {
+            println!("sheet split_cells response: {:#?}", base_resp.data);
+        }
+        Err(err) => {
+            println!("sheet split_cells error: {:#?}", err);
+        }
+    }
+}
