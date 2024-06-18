9ca2662ebe38d57bd456fb651f808b710fd15fcb
ZoOL <zhooul@gmail.com>
2024-06-18 13:55:08 +0800

    电子表格 - 删除行列

--- examples/api/sheets/v2/spreadsheet/delete_row_or_columns.rs
+++ examples/api/sheets/v2/spreadsheet/delete_row_or_columns.rs
@@ -4,10 +4,10 @@ use dotenvy::dotenv;
 
 use open_lark::{
     client::LarkClientBuilder,
-    service::sheets::v2::sheet_row_col::AddDimensionRangeRequest,
+    service::sheets::v2::sheet_row_col::DeleteDimensionRangeRequest,
 };
 
-/// 操作工作表
+/// 删除行列
 #[tokio::main]
 async fn main() {
     dotenv().expect(".env file not found");
@@ -17,22 +17,22 @@ async fn main() {
     // 创建 Client
     let client = LarkClientBuilder::new(&app_id, &app_secret).build();
 
-
-    let req = AddDimensionRangeRequest::builder()
+    let req = DeleteDimensionRangeRequest::builder()
         .spreadsheet_token("O21wsTInWht7sUtRj77cFwRXnme")
         .sheet_id("0ae03b")
         .major_dimension("ROWS")
-        .length(1)
+        .start_index(4)
+        .end_index(4)
         .build();
     let resp = client
         .sheets
         .v2
         .spreadsheet
-        .add_dimension_range(req, None)
+        .delete_dimension_range(req, None)
         .await
         .unwrap();
 
     if let Some(data) = resp.data {
-        println!("add-rows-or-columns spreadsheet response: {:#?}", data);
+        println!("update-rows-or-columns spreadsheet response: {:#?}", data);
     }
 }
