1b7485c5de3cfd1c493b48df775001c85c964898
ZoOL <zhooul@gmail.com>
2024-06-18 11:30:24 +0800

    电子表格 - 增加行列

---
+++ examples/api/sheets/v2/spreadsheet/add_row_or_columns.rs
@@ -0,0 +1,38 @@
+use std::env;
+
+use dotenvy::dotenv;
+
+use open_lark::{
+    client::LarkClientBuilder,
+    service::sheets::v2::sheet_row_col::DimensionRangeRequest,
+};
+
+/// 操作工作表
+#[tokio::main]
+async fn main() {
+    dotenv().expect(".env file not found");
+    env_logger::init();
+    let app_id = env::var("APP_ID").unwrap();
+    let app_secret = env::var("APP_SECRET").unwrap();
+    // 创建 Client
+    let client = LarkClientBuilder::new(&app_id, &app_secret).build();
+
+    // 查询原测试工作表的sheet id
+    let req = DimensionRangeRequest::builder()
+        .spreadsheet_token("O21wsTInWht7sUtRj77cFwRXnme")
+        .sheet_id("0ae03b")
+        .major_dimension("ROWS")
+        .length(1)
+        .build();
+    let resp = client
+        .sheets
+        .v2
+        .spreadsheet
+        .dimension_range(req, None)
+        .await
+        .unwrap();
+
+    if let Some(data) = resp.data {
+        println!("add-rows-or-columns spreadsheet response: {:#?}", data);
+    }
+}
