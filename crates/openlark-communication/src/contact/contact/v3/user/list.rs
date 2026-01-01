eced59f79fed1897f8bdc807d8d037aeeb1f4779
ZoOL <zhooul@gmail.com>
2026-01-01 23:30:09 +0800

    🐛 fix: refresh broken contact docPath links

--- crates/openlark-communication/src/contact/contact/v3/user/list.rs
+++ crates/openlark-communication/src/contact/contact/v3/user/list.rs
@@ -1,6 +1,6 @@
 //! 获取用户列表
 //!
-//! docPath: https://open.feishu.cn/document/server-docs/historic-version/user/list
+//! docPath: https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/contact-v3/user/list
 
 use openlark_core::{
     api::{ApiRequest, ApiResponseTrait, ResponseFormat},
@@ -87,7 +87,7 @@ impl ListUsersRequest {
 
     /// 执行请求
     ///
-    /// docPath: https://open.feishu.cn/document/server-docs/historic-version/user/list
+    /// docPath: https://open.feishu.cn/document/uAjLw4CM/ukTMukTMukTM/reference/contact-v3/user/list
     pub async fn execute(self) -> SDKResult<ListUsersResponse> {
         // url: GET:/open-apis/contact/v3/users
         let mut req: ApiRequest<ListUsersResponse> = ApiRequest::get(CONTACT_V3_USERS);
