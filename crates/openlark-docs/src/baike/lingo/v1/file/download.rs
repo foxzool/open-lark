7519a1ea4a27410f8e798c420e057671bf88579b
ZoOL <zhooul@gmail.com>
2026-01-27 01:36:58 +0800

    🔧 fix(docs): wrap lingo download in Response

--- crates/openlark-docs/src/baike/lingo/v1/file/download.rs
+++ crates/openlark-docs/src/baike/lingo/v1/file/download.rs
@@ -3,8 +3,8 @@
 //! docPath: https://open.feishu.cn/document/lingo-v1/file/download
 
 use openlark_core::{
-    api::ApiRequest, config::Config, http::Transport, req_option::RequestOption, validate_required,
-    SDKResult,
+    api::{ApiRequest, Response}, config::Config, http::Transport, req_option::RequestOption,
+    validate_required, SDKResult,
 };
 
 use crate::common::api_endpoints::LingoApiV1;
@@ -25,18 +25,18 @@ impl DownloadFileRequest {
     }
 
     /// 下载图片二进制内容
-    pub async fn execute(self) -> SDKResult<Vec<u8>> {
+    pub async fn execute(self) -> SDKResult<Response<Vec<u8>>> {
         self.execute_with_options(RequestOption::default()).await
     }
 
     /// 下载图片二进制内容（支持自定义选项）
-    pub async fn execute_with_options(self, option: RequestOption) -> SDKResult<Vec<u8>> {
+    pub async fn execute_with_options(self, option: RequestOption) -> SDKResult<Response<Vec<u8>>> {
         validate_required!(self.file_token, "file_token 不能为空");
 
         let api_request: ApiRequest<Vec<u8>> =
             ApiRequest::get(&LingoApiV1::FileDownload(self.file_token).to_url());
 
         let response = Transport::request(api_request, &self.config, Some(option)).await?;
-        extract_response_data(response, "下载图片")
+        Ok(response)
     }
 }
